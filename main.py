4fd57b7774423378f47074a2e892b12264d78a8c
Emperor <108576485+Joseph-67@users.noreply.github.com>
2023-07-27 12:50:46 +0100

    update main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import speech_recognition as sr
+
+# create recognizer
+def CommandListener():
+    #create the speech recognizer
+    recognizer = sr.Recognizer()
+    print("Listening for speech...")
+
+    with sr.Microphone() as source:
+      audio = recognizer.listen(source)
+      query = recognizer.recognize_google(audio)
+      print(f'Listener says:{query}')     
+
+ # functin invocation 
+      CommandListener()
